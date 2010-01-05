002f9ac7cf0fd806c3d5e625c1c760601ab1b07f
j1n3l0 <nelo.onyiah@gmail.com>
2010-01-05 16:46:35 +0000

    Sort the features.

--- grid.rb
+++ grid.rb
@@ -4,7 +4,10 @@ class Grid
   attr_reader :features, :primers
 
   def initialize(features)
-    @features = features
+    @features = features.sort do |a,b|
+      a["position"] <=> b["position"]
+    end
+
     @primers  = @features.select do |x|
       x["name"] == "rcmb_primer"
     end
--- test.rb
+++ test.rb
@@ -7,9 +7,13 @@ require "grid"
 class TestGrid < Test::Unit::TestCase
   context "a new Grid" do
     setup do
+      # Generating the feature list will be factored out as well
       @features = [
-        { "name" => "rcmb_primer" },
-        {}, {}, {}, {}
+        { "name" => "rcmb_primer", "position" => 80 },
+        { "position" => 10 },
+        { "position" => 60 },
+        { "position" => 100 },
+        { "position" => 110 }
       ]
       
       @grid = Grid.new(@features)
@@ -17,6 +21,11 @@ class TestGrid < Test::Unit::TestCase
     
     should "have some features" do
       assert_equal(5, @grid.features.count)
+    end
+
+    should "have sorted features" do
+      assert_equal( 10, @grid.features.first["position"])
+      assert_equal(110, @grid.features.last["position"] )
     end
 
     should "have some rcmb primers" do
