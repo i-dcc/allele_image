d965369041dffb2688b1f8194dc53581c04c17a1
j1n3l0 <nelo.onyiah@gmail.com>
2010-01-06 15:21:36 +0000

    Added annotation and main feature attributes.

--- grid.rb
+++ grid.rb
@@ -2,9 +2,13 @@
 
 class Grid
   attr_reader :features, :rcmb_primers, :rows, :is_circular
+  attr_reader :annotation_features, :main_features
 
   # Not 100% sure if a Grid should have/allow access to @features and @rcmb_primers
   def initialize(features, is_circular)
+    @is_circular = is_circular
+
+    # Manage the features
     @features = features.sort do |a,b|
       a.position <=> b.position
     end
@@ -13,7 +17,18 @@ class Grid
       x.name == "rcmb_primer"
     end
 
-    @is_circular = is_circular
+    @annotation_features = @features.select do |x|
+         x.name.downcase == 'rcmb_primer'  \
+      or x.name.downcase == 'lrpcr_primer' \
+      or x.name.downcase == 'genomic'
+    end
+
+    @main_features = @features.select do |x|
+          x.name.downcase != 'rcmb_primer'  \
+      and x.name.downcase != 'lrpcr_primer' \
+      and x.name.downcase != 'genomic'      \
+      and x.name.downcase != 'primer_bind'
+    end
 
     @rows = 3
     @rows = 5 if @is_circular == 1
