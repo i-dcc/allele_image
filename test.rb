0c142840f319fd6a019ab5779542cc72919df0e9
j1n3l0 <nelo.onyiah@gmail.com>
2010-01-06 13:56:55 +0000

    Fleshed out the tests for Section class.

--- test.rb
+++ test.rb
@@ -68,16 +68,14 @@ class TestGrid < Test::Unit::TestCase
   # Test the Section class
   context "a new Section" do
     setup do
-      @section = Section.new()
+      @section = Section.new(0, [])
     end
 
-    should "have an index" do
-    end
-
-    should "have a bunch of features" do
-    end
-
-    should "have bounding primers (at least one)" do
+    should "have an index, a bunch of features and bounding primers (at least one)" do
+      assert_equal(0, @section.index)
+      assert_equal(0, @section.features.size)
+      assert_equal(nil, @section.primer_5)
+      assert_equal(nil, @section.primer_3)
     end
   end
 
