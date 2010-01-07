05f279b14537c497ed2accd0adc7adc9de5e4250
j1n3l0 <nelo.onyiah@gmail.com>
2010-01-07 11:31:32 +0000

    Use @rcmb_primers to classify features into sections.

--- row.rb
+++ row.rb
@@ -2,16 +2,42 @@
 require "section"
 
 class Row
-  attr_reader :index, :features, :sections
+  attr_reader :index, :features, :sections, :rcmb_primers
 
   def initialize(index, features)
     @index, @features = index, features
+
+    @rcmb_primers = @features.select do |x|
+      x.name == "rcmb_primer"
+    end
     
     # Allocate the features into their sections
-    @sections = []
+    @sections = Array.new()
+    
+    # 5' flank region
+    @sections[0] = @features.select do |x|
+      x.position < @rcmb_primer.select{ |y| y.label == "G5" }.position
+    end
+    
+    # 5' homology region
+    @sections[1] = @features.select do |x|
+          x.position > @rcmb_primer.select{ |y| y.label == "G5" }.position \
+      and x.position < @rcmb_primer.select{ |y| y.label == "U5" }.position
+    end
+    
+    # cassette region
+    @sections[2] = @features.select do |x|
+          x.position > @rcmb_primer.select{ |y| y.label == "U5" }.position \
+      and x.position < @rcmb_primer.select{ |y| y.label == "U3" }.position
+    end
+    
+    # target region
+    # loxP region
+    # 3' homology
+    # 3' flank region
   end
   
-  # Returns a Section
-  def merge_sections(a, b)
-  end
+  # # Returns a Section
+  # def merge_sections(a, b)
+  # end
 end
--- test-row.rb
+++ test-row.rb
@@ -5,25 +5,24 @@ require "shoulda"
 require "row"
 
 class TestRow < Test::Unit::TestCase
-  # Test the Row class
   context "a new Row" do
     setup do
       @row = Row.new(0, []) # index, sections
     end
 
-    # should "know which Grid it belongs to (I think)" do
-    # end
-
     should "allow access to its Sections" do
-      assert_equal(0, @row.sections.size)
+      assert_equal(0, @row.sections[0].size)
     end
 
     should "allow access to its index" do
       assert_equal(0, @row.index)
     end
 
-    should "be able to merge 2 Sections" do
-      # write a test
+    should "have 4 to 6 recombineering primers" do
+      assert_equal(0, @row.rcmb_primers.size)
     end
+
+    # should "be able to merge 2 Sections" do
+    # end
   end
 end
