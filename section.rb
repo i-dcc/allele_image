ac7f57cb0781af3b181e5b35a754ade9a1cc4377
j1n3l0 <nelo.onyiah@gmail.com>
2010-01-08 11:46:02 +0000

    Refactored Section class such that the bounding primers have to be provided (or nil). Expanded the tests a bit.

--- section.rb
+++ section.rb
@@ -6,23 +6,20 @@ class Section
   def initialize(index, features, lower_primer, upper_primer)
     @index, @lower_primer, @upper_primer = index, lower_primer, upper_primer
 
-    @rcmb_primers = features.select do |x|
-      x.name == "rcmb_primer"
-    end
-    
     if @lower_primer and @upper_primer
-      @features = features.select do |x|
-            x.position > @rcmb_primer.select{ |y| y.label == @lower_primer }.position \
-        and x.position < @rcmb_primer.select{ |y| y.label == @upper_primer }.position
+      @features = features.select do |feature|
+        feature.position > lower_primer.position and feature.position < upper_primer.position
       end
     elsif @lower_primer and not @upper_primer
-      @features = features.select do |x|
-        x.position > @rcmb_primer.select{ |y| y.label == @lower_primer }.position
+      @features = features.select do |feature|
+        feature.position > lower_primer.position
       end
-    elsif @upper_primer and not @lower_primer
-      @features = features.select do |x|
-        x.position < @rcmb_primer.select{ |y| y.label == @upper_primer }.position
+    elsif not @lower_primer and @upper_primer
+      @features = features.select do |feature|
+        feature.position < upper_primer.position
       end
+    else
+      raise "At least one bounding primer required"
     end
   end
 end
--- test-section.rb
+++ test-section.rb
@@ -3,25 +3,40 @@ require "rubygems"
 require "test/unit"
 require "shoulda"
 require "section"
+require "feature"
 
 class TestSection < Test::Unit::TestCase
   context "a new Section" do
     setup do
-      # Need to test with non-empty data
-      @section = Section.new(0, [], "G5", "U5")
+      @features = [
+        Feature.new("exon", 100, "EXON001"),
+        Feature.new("exon", 200, "EXON002"),
+        Feature.new("exon", 300, "EXON003"),
+        Feature.new("exon", 400, "EXON004"),
+        Feature.new("exon", 900, "EXON009")
+      ]
+      @primers = [
+        Feature.new("rcmb_primer", 150, "G5"),
+        Feature.new("rcmb_primer", 350, "U5")
+      ]
+      @section1 = Section.new(0, @features, nil, @primers.first)
+      @section2 = Section.new(1, @features, @primers.first, @primers.last)
+      @section3 = Section.new(2, @features, Feature.new("rcmb_primer", 700, "G3"), nil)
     end
 
     should "have an index" do
-      assert_equal(0, @section.index)
+      assert_equal(1, @section2.index)
     end
-
+    
     should "have a bunch of features" do
-      assert_equal(0, @section.features.size)
+      assert_equal(1, @section1.features.size)
+      assert_equal(2, @section2.features.size)
+      assert_equal(1, @section3.features.size)
     end
 
     should "have bounding primers (at least one)" do
-      assert_equal("G5", @section.lower_primer)
-      assert_equal("U5", @section.upper_primer)
+      assert_equal("G5", @section2.lower_primer.label)
+      assert_equal("U5", @section2.upper_primer.label)
     end
   end
 end
