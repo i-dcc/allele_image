400637d6d2ca100ecd20e21b2e78dd23394f708b
j1n3l0 <nelo.onyiah@gmail.com>
2010-01-08 09:55:12 +0000

    Corrected the indexes. Mot happy with indexes at all though.

--- row.rb
+++ row.rb
@@ -14,13 +14,14 @@ class Row
     # Allocate the features into their sections
     @sections = Array.new()
 
+    # Don't like the indexing going on. Should be implicit (I think)
     @sections.push( Section.new(0, @features, nil , "G5" ) ) # 5' flank region
-    @sections.push( Section.new(0, @features, "G5", "U5" ) ) # 5' homology region
-    @sections.push( Section.new(0, @features, "U5", "U3" ) ) # cassette region
-    @sections.push( Section.new(0, @features, "U3", "D5" ) ) # target region
-    @sections.push( Section.new(0, @features, "D5", "D3" ) ) # loxP region
-    @sections.push( Section.new(0, @features, "D3", "G3" ) ) # 3' homology
-    @sections.push( Section.new(0, @features, "G3", nil  ) ) # 3' flank region
+    @sections.push( Section.new(1, @features, "G5", "U5" ) ) # 5' homology region
+    @sections.push( Section.new(2, @features, "U5", "U3" ) ) # cassette region
+    @sections.push( Section.new(3, @features, "U3", "D5" ) ) # target region
+    @sections.push( Section.new(4, @features, "D5", "D3" ) ) # loxP region
+    @sections.push( Section.new(5, @features, "D3", "G3" ) ) # 3' homology
+    @sections.push( Section.new(6, @features, "G3", nil  ) ) # 3' flank region
   end
   
   # # Returns a Section
