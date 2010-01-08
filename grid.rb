cd1718e09c332a558530112391d61363bce42879
j1n3l0 <nelo.onyiah@gmail.com>
2010-01-08 09:57:31 +0000

    Removed @rcmb_primers from Grid class.

--- grid.rb
+++ grid.rb
@@ -3,7 +3,6 @@ require "row"
 
 class Grid
   attr_reader :features, :is_circular, :rows
-  # attr_reader :rcmb_primers # not sure if this needs to be explicit
   attr_reader :annotation_features, :main_features
 
   # Not 100% sure if a Grid should have/allow access to @features and @rcmb_primers
@@ -14,10 +13,6 @@ class Grid
     @features = features.sort do |a,b|
       a.position <=> b.position
     end
-
-    # @rcmb_primers = @features.select do |x|
-    #   x.type == "rcmb_primer"
-    # end
 
     @annotation_features = @features.select do |x|
          x.type.downcase == "rcmb_primer"  \
