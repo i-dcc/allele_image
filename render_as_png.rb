e447793b9d1d77fed23c0521a4d8e25850e535f0
j1n3l0 <nelo.onyiah@gmail.com>
2010-02-02 16:34:10 +0000

    Moved the tests out of the class definition

--- render_as_png.rb
+++ render_as_png.rb
@@ -36,26 +36,3 @@ class RenderAsPNG
   def render_grid(params)
   end
 end
-
-require "feature"
-require "section"
-
-primers = [
-  Feature.new("rcmb_primer", 150, "G5"),
-  Feature.new("rcmb_primer", 350, "U5"),
-  Feature.new("rcmb_primer", 450, "D3"),
-  Feature.new("rcmb_primer", 700, "G3")
-]
-features = [
-  Feature.new("exon", 100, "EXON001"),
-  Feature.new("exon", 200, "EXON002"),
-  Feature.new("exon", 300, "EXON003"),
-  Feature.new("exon", 400, "EXON004"),
-  Feature.new("exon", 900, "EXON009")
-]
-section = Section.new(0, features, primers[0], primers[1])
-section.render(RenderAsPNG, :width => 200, :height => 100)
-
-# i = Image.new(200, 100)
-# f = Feature.new("exon", 10, "EXON001").render(RenderAsPNG, :section => i, :x1 => 50, :y1 => 25, :x2 => 150, :y2 => 75)
-# i.write("test.png")
