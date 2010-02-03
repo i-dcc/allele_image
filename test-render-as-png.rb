dc484d21a6b8e8fe371e4aa0db5b39375a0d065f
j1n3l0 <nelo.onyiah@gmail.com>
2010-02-03 12:03:21 +0000

    Added a test for the type of the rendered section.

--- test-render-as-png.rb
+++ test-render-as-png.rb
@@ -14,13 +14,37 @@ class TestRenderAsPNG < Test::Unit::TestCase
   context "a new Feature" do
     setup do
       @feature = Feature.new("exon", 100, "EXON001")
+      @format  = RenderAsPNG
       @section = Image.new(200, 100)
     end
 
+    # needs more thourough tests
     should "render itself as a Magick::Draw object" do
-      rendered_feature = @feature.render(RenderAsPNG, :x1 => 50, :x2 => 150, :y1 => 25, :y2 => 75, :section => @section)
+      rendered_feature = @feature.render(@format, :x1 => 50, :x2 => 150, :y1 => 25, :y2 => 75, :section => @section)
       # @section.write("feature.png")
       assert_equal(rendered_feature.class, Magick::Draw)
+    end
+  end
+
+  context "a new Section" do
+    setup do
+      @features = [
+        Feature.new("exon", 100, "EXON001"),
+        Feature.new("exon", 200, "EXON002"),
+        Feature.new("exon", 300, "EXON003"),
+        Feature.new("exon", 400, "EXON004"),
+        Feature.new("exon", 900, "EXON009")
+      ]
+      @primers = [
+        Feature.new("rcmb_primer", 150, "G5"),
+        Feature.new("rcmb_primer", 350, "U5")
+      ]
+      @section = Section.new(0, @features, @primers.first, @primers.last)
+      @format  = RenderAsPNG
+    end
+
+    should "render itself as a Magick::Image object" do
+      assert_equal(@section.render(@format), Magick::Image)
     end
   end
 end
