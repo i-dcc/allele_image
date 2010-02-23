a491373077e0d63457a4fde8544f482010ea363d
j1n3l0 <nelo.onyiah@gmail.com>
2010-02-23 17:17:17 +0000

    Rendering the homology arms correctly (with extremely hideous code that will get cleaned up ASAP)!

--- render_as_png.rb
+++ render_as_png.rb
@@ -35,26 +35,26 @@ class RenderAsPNG
 
     if params[:row_number] == 0
       case @thing.type
-        when "genomic" then
-          if params[:bounding_primers].size == 2
-            # The homology arm drawn should depend on:
-            # 1 - the bounding primers
-            # 2 - the total number of primers
-            # Essentially, there are standard images to be drawn between different
-            # pairs of primers with the only variabilty being b/w D5 and D3. This
-            # would be dependent on the presence of a U3 primer upstream of the D5.
-            case params[:bounding_primers].map { |p| p.label }
-              when ["G5", "U5"] then draw_G5_U5(d, params)
-              when ["U3", "D5"] then draw_U3_D5(d, params)
-              when ["D5", "D3"] then draw_D5_D3(d, params)
-              when ["D3", "G3"] then draw_D3_G3(d, params)
-              else
-                unless ["U5", "U3"].eql?( params[:bounding_primers].map { |p| p.label } )
-                  # puts ""
-                  # pp [ "NOT HANDLED YET:", { :bounding_primers => [ params[:bounding_primers].map { |x| x.label } ] } ]
-                end
-            end
-          end
+        # when "genomic" then
+        #   if params[:bounding_primers].size == 2
+        #     # The homology arm drawn should depend on:
+        #     # 1 - the bounding primers
+        #     # 2 - the total number of primers
+        #     # Essentially, there are standard images to be drawn between different
+        #     # pairs of primers with the only variabilty being b/w D5 and D3. This
+        #     # would be dependent on the presence of a U3 primer upstream of the D5.
+        #     case params[:bounding_primers].map { |p| p.label }
+        #       when ["G5", "U5"] then draw_G5_U5(d, params)
+        #       when ["U3", "D5"] then draw_U3_D5(d, params)
+        #       when ["D5", "D3"] then draw_D5_D3(d, params)
+        #       when ["D3", "G3"] then draw_D3_G3(d, params)
+        #       else
+        #         unless ["U5", "U3"].eql?( params[:bounding_primers].map { |p| p.label } )
+        #           # puts ""
+        #           # pp [ "NOT HANDLED YET:", { :bounding_primers => [ params[:bounding_primers].map { |x| x.label } ] } ]
+        #         end
+        #     end
+        #   end
         when "LRPCR_primer" then
           # draw_lrpcr_primer(d, params)
         else
@@ -62,8 +62,15 @@ class RenderAsPNG
           # pp [ "NOT HANDLED YET:", { :LRPCR_primer => @thing } ]
       end
     elsif params[:row_number] == 1
-      puts
-      pp [ "ORIGIN", { :x1 => params[:x1], :y1 => params[:y1], :section => params[:section_index] } ]
+      if params[:section_index] == 6
+        # puts
+        # pp [
+        #   "ORIGIN",
+        #   { :x1 => params[:x1], :y1 => params[:y1], :section => params[:section_index],
+        #     :section => params[:section], :thing => @thing }
+        # ]
+      end
+
       # Here we should be checking the type of the feature and delegating to
       # specific methods for rendering those features.
       # All these methods should be refactored out and should be of the form:
@@ -79,8 +86,8 @@ class RenderAsPNG
             when "neo"             then draw_neo(d, params)
             when /en.*2\s+intron/i then draw_en2_sa(d, params)
             else
-              puts ""
-              pp   [ "UNKNOWN CASSETTE FEATURE:", { :row => params[:row_number], :feature => @thing } ]
+              # puts ""
+              # pp   [ "UNKNOWN CASSETTE FEATURE:", { :row => params[:row_number], :feature => @thing } ]
               # raise "Unkown cassette feature"
           end
         when "SSR_site"     then
@@ -128,7 +135,7 @@ class RenderAsPNG
     max_feature_length = feature_lengths.length > 0 ? feature_lengths.max : 0
 
     params[:section_index] = @thing.index
-    features_to_render = @thing.features
+    features_to_render     = params[:renderable_features]
 
     exons = @thing.features.select do |f|
       f.type == 'exon'
@@ -163,12 +170,26 @@ class RenderAsPNG
       params[:gap] = 5
 
       # centering the images
-      feature_total_width = ( params[:feature_width] * params[:renderable_features].size ) + ( params[:gap] * ( params[:renderable_features].size - 1 ) )
+      feature_total_width = ( params[:feature_width] * features_to_render.size ) + ( params[:gap] * ( params[:renderable_features].size - 1 ) )
       feature_total_width = 0 unless feature_total_width > 0
       params[:x1]         = ( params[:width] - feature_total_width ) / 2
       params[:y1]         = ( params[:height] - params[:feature_height] ) / 2
       params[:x2]         = params[:x1] + params[:feature_width]
       params[:y2]         = params[:y1] + params[:feature_height]
+
+      if @thing.index == 6
+        # puts
+        # pp [ "WHERE DOES X1 GET SET:", {
+        #   :feature_width => params[:feature_width],
+        #   :renderable_features => params[:renderable_features].size,
+        #   :gap => params[:gap],
+        #   :feature_total_width_orig => ( params[:feature_width] * features_to_render.size ) + ( params[:gap] * ( params[:renderable_features].size - 1 ) ),
+        #   :feature_total_width => feature_total_width,
+        #   :section => @thing.index, :row => params[:row_number],
+        #   :width => params[:width],
+        #   :x1 => params[:x1], :y1 => params[:y1]
+        # } ]
+      end
     end
 
     params[:section] = Image.new( params[:width], params[:height] )
@@ -180,6 +201,11 @@ class RenderAsPNG
         d.stroke_width(2.5)
         d.line( 0, params[:height] / 2, params[:width], params[:height] / 2 )
       end
+    end
+
+    if params[:x1] < 0
+      # puts ""
+      # pp [ "SECTION X1 < 0" => @thing ]
     end
 
     # loop through and render our features ...
@@ -273,16 +299,55 @@ class RenderAsPNG
     grid = grid.append(true)
 
     # perhaps we can label both the homology arms here?
+    # Label the 5' homology arm
+    five_arm = Draw.new
+    five_arm.annotate(
+      grid,
+      params[:widths][1],
+      params[:text_height],
+      params[:widths][0],
+      params[:height] / 2 - params[:text_height],
+      "5' homology arm (#{params[:rcmb_primers][1].stop - params[:rcmb_primers][0].start})"
+    ) { self.gravity = CenterGravity }
+    five_arm.stroke("black")
+    five_arm.stroke_width(2.5)
+    five_arm.line( params[:widths][0], params[:height]/2 + params[:height]/20, params[:widths][0], params[:height]/2 ).draw(grid)
+    five_arm.line( params[:widths][0], params[:height]/2, params[:widths][1], params[:height]/2 ).draw(grid)
+    five_arm.line( params[:widths][1], params[:height]/2, params[:widths][1], params[:height]/2 + params[:height]/20 ).draw(grid)
+
     # Label the 3' homology arm
-    d = Draw.new
-    d.annotate(
+    three_arm = Draw.new
+    three_arm.annotate(
       grid,
       eval( ( params[:rcmb_primers].size == 4 ? params[:widths][3,2] : params[:widths][3,3] ).join('+') ),
       params[:text_height],
       eval( params[:widths][0,3].join('+') ),
-      100 / 2 - params[:text_height],
+      params[:height] / 2 - params[:text_height],
       "3' homology arm (#{params[:rcmb_primers].last.stop - params[:rcmb_primers][2].start})"
     ) { self.gravity = CenterGravity }
+    three_arm.stroke("black")
+    three_arm.stroke_width(2.5)
+    three_arm.line( params[:widths][0,3].reduce { |a,b| a + b }, params[:height]/2 + params[:height]/20, params[:widths][0,3].reduce { |a,b| a + b }, params[:height]/2 ).draw(grid)
+    three_arm.line( params[:widths][0,3].reduce { |a,b| a + b }, params[:height]/2, params[:widths][0,3].reduce { |a,b| a + b } + ( params[:rcmb_primers].size == 4 ? params[:widths][3,2] : params[:widths][3,3] ).reduce { |a,b| a + b }, params[:height]/2 ).draw(grid)
+    three_arm.line( params[:widths][0,3].reduce { |a,b| a + b } + ( params[:rcmb_primers].size == 4 ? params[:widths][3,2] : params[:widths][3,3] ).reduce { |a,b| a + b }, params[:height]/2, params[:widths][0,3].reduce { |a,b| a + b } + ( params[:rcmb_primers].size == 4 ? params[:widths][3,2] : params[:widths][3,3] ).reduce { |a,b| a + b }, params[:height]/2 + params[:height]/20 ).draw(grid)
+
+    pp [
+      [ 
+        # params[:widths][0,3].reduce { |a,b| a + b }, params[:height]/2 + params[:height]/20
+        # eval( params[:widths][0,3] )#.join("+"), params[:height]/2 + params[:height]/20,
+        # eval( params[:widths][0,3] ).join("+"), params[:height]/2
+      ]
+    ]
+
+    pp [
+      # params[:widths],
+      grid,
+      eval( ( params[:rcmb_primers].size == 4 ? params[:widths][3,2] : params[:widths][3,3] ).join('+') ),
+      params[:text_height],
+      eval( params[:widths][0,3].join('+') ),
+      params[:height] / 2 - params[:text_height],
+      "3' homology arm (#{params[:rcmb_primers].last.stop - params[:rcmb_primers][2].start})"
+    ]
 
     grid
   end
