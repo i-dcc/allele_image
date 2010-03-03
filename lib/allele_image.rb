8b399d5843aed57de2d7ffcba2c4c876f3b42b21
j1n3l0 <nelo.onyiah@gmail.com>
2010-03-03 17:08:34 +0000

    Rendering the image is wrapped in a begin-rescue block because of Magick::ImageMagickErrors

--- lib/allele_image.rb
+++ lib/allele_image.rb
@@ -12,10 +12,10 @@ module AlleleImage
 
   # write the image to a file
   def write_to_file( file )
-    # this should also be in a try-catch block itself as sometimes we can't get the fonts
-    image = self.render()
     begin
-      image.write( file )
+      self.render().write( file )
+    rescue Magick::ImageMagickError => error
+      puts "Magick::ImageMagickError: " + error
     rescue
       puts "Problems writing image to file: #{ file }"
     end
