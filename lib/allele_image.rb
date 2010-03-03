925debb4b641e9b439beb8a704f7ef72d9c8c28e
j1n3l0 <nelo.onyiah@gmail.com>
2010-03-03 16:27:32 +0000

    Re-structured code to follow more sensible guidelines

--- lib/allele_image.rb
+++ lib/allele_image.rb
@@ -1,6 +1,16 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require "rubygems"
+require "RMagick"
+require "pp"
+
+include Magick
+
 module AlleleImage
   VERSION = '0.0.1'
 end
+
+directory = File.expand_path( File.dirname(__FILE__) )
+
+require File.join( directory, 'allele_image', 'cassette_region' )
