801445e616c406a40010c89c65fdb50c4b78dc80
j1n3l0 <nelo.onyiah@gmail.com>
2010-03-05 18:43:35 +0000

    Create a new AlleleImage::Image based on the format of the input file, which we are currently just going to set to "GenBank".

--- lib/allele_image.rb
+++ lib/allele_image.rb
@@ -8,17 +8,33 @@ require "pp"
 
 include Magick
 
+# This entire module could do with a better name
 module AlleleImage
   VERSION = '0.0.1'
 
-  # write the image to a file
-  def write_to_file( file )
-    begin
-      self.render().write( file )
-    rescue Magick::ImageMagickError => error
-      puts "Magick::ImageMagickError: " + error
-    rescue
-      puts "Problems writing image to file: #{ file }"
+  class Image
+    attr_reader :features, :rcmb_primers, :cassette_features, :five_homology_features
+
+    # Need a smarter way of obtaining the format (in the long run) than supplying an argument
+    # Should also (for example) be able to instantiate a new AlleleImage::Image from a list of
+    # features
+    def initialize( file, input_format = "Genbank" )
+      parse_result            = eval( "AlleleImage::Parse::#{ input_format }" ).new( file )
+      @features               = parse_result.features
+      @rcmb_primers           = parse_result.rcmb_primers
+      @cassette_features      = parse_result.cassette_features
+      @five_homology_features = parse_result.five_homology_features
+    end
+
+    # write the image to a file
+    def write_to_file( file )
+      begin
+        self.render().write( file )
+      rescue Magick::ImageMagickError => error
+        puts "Magick::ImageMagickError: " + error
+      rescue
+        puts "Problems writing image to file: #{ file }"
+      end
     end
   end
 end
--- test/test_allele_image.rb
+++ test/test_allele_image.rb
@@ -1,11 +1,31 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestAlleleImage < Test::Unit::TestCase
+  context "a new AlleleImage::Image" do
+    setup do
+      @file = File.dirname(__FILE__) + "/../misc/project_47462_conditional_allele.gbk"
+      @allele_image = AlleleImage::Image.new( @file )
+    end
 
-  def setup
-  end
-  
-  def test_truth
-    assert true
+    should "instantiate" do
+      assert_not_nil @allele_image
+    end
+
+    should "return the correct number of features" do
+      assert_equal 69, @allele_image.features.count
+    end
+
+    should "return the correct number of rcmb_primers" do
+      assert_equal 6, @allele_image.rcmb_primers.count
+    end
+
+    should "return the correct number of cassette features" do
+      # check what the correct number for this file should be and adjust
+      assert_equal 42, @allele_image.cassette_features.count
+    end
+
+    should "return the correct number of 5' arm features" do
+      assert_equal 6, @allele_image.five_homology_features.count
+    end
   end
 end
