2ca91eb47c1f48758346241de374bb305b8f45e2
j1n3l0 <nelo.onyiah@gmail.com>
2010-03-05 16:29:29 +0000

    Test writing the features from the parse to file

--- test/test_parse_genbank.rb
+++ test/test_parse_genbank.rb
@@ -3,7 +3,9 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestAlleleImageParseGenbank < Test::Unit::TestCase
   context "a new GenBank parser" do
     setup do
-      @parser = AlleleImage::Parse::Genbank.new( File.dirname(__FILE__) + "/../misc/project_47462_conditional_allele.gbk" )
+      @file     = File.dirname(__FILE__) + "/../misc/project_47462_conditional_allele.gbk"
+      @parser   = AlleleImage::Parse::Genbank.new( @file )
+      @cassette = AlleleImage::CassetteRegion.new( @parser.cassette_features )
     end
 
     should "return true on construction" do
@@ -21,6 +23,10 @@ class TestAlleleImageParseGenbank < Test::Unit::TestCase
     should "return the correct number of cassette features" do
       # check what the correct number for this file should be and adjust
       assert_equal 42, @parser.cassette_features.count
+    end
+
+    should "write to a PNG file returning a Magick::Image" do
+      assert_instance_of Magick::Image, @cassette.write_to_file( "/nfs/users/nfs_i/io1/tmp/project_47462_conditional_allele.png" )
     end
   end
 end
