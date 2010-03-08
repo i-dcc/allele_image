bc2f04abe483aa570f7ad6f8be45c978431a9e2c
j1n3l0 <nelo.onyiah@gmail.com>
2010-03-08 16:37:15 +0000

    Minor refactor

--- lib/allele_image/three_homology_region.rb
+++ lib/allele_image/three_homology_region.rb
@@ -6,30 +6,30 @@ module AlleleImage
     def initialize( features )
       @features     = features
       @rcmb_primers = @features.select { |feature| feature[:type] == "rcmb_primer" }
+
+      if @rcmb_primers.count == 2
+        @three_arm_features     = @features
+        @target_region_features = nil
+        @loxp_region_features   = nil
+      else
+        @target_region_features = @features.select do |feature|
+          feature[:start] >= @rcmb_primers[0][:start] and feature[:start] <= @rcmb_primers[1][:start]
+        end
+        @loxp_region_features = @features.select do |feature|
+          feature[:start] >= @rcmb_primers[1][:start] and feature[:start] <= @rcmb_primers[2][:start] and feature[:type] == "SSR_site"
+        end
+        @three_arm_features = @features.select do |feature|
+          feature[:start] >= @rcmb_primers[2][:start] and feature[:start] <= @rcmb_primers[3][:start]
+        end
+      end
     end
 
     def render
       image = Magick::ImageList.new
 
-      if @rcmb_primers.count == 2
-        image.push( AlleleImage::FiveHomologyRegion.new( @features ).render() )
-      else
-        image.push( AlleleImage::FiveHomologyRegion.new(
-          @features.select do |feature|
-            feature[:start] >= @rcmb_primers[0][:start] and feature[:start] <= @rcmb_primers[1][:start]
-          end
-        ).render() )
-        image.push( AlleleImage::CassetteRegion.new(
-          @features.select do |feature|
-            feature[:start] >= @rcmb_primers[1][:start] and feature[:start] <= @rcmb_primers[2][:start] and feature[:type] == "SSR_site"
-          end
-        ).render() )
-        image.push( AlleleImage::FiveHomologyRegion.new(
-          @features.select do |feature|
-            feature[:start] >= @rcmb_primers[2][:start] and feature[:start] <= @rcmb_primers[3][:start]
-          end
-        ).render() )
-      end
+      image.push( AlleleImage::FiveHomologyRegion.new( @three_arm_features ).render() )
+      image.unshift( AlleleImage::CassetteRegion.new( @loxp_region_features ).render() )       unless @loxp_region_features.nil?
+      image.unshift( AlleleImage::FiveHomologyRegion.new( @target_region_features ).render() ) unless @target_region_features.nil?
 
       image.append( false )
     end
