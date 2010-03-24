181b6e0106e1330165f1c122db9f9c24a8921b81
j1n3l0 <nelo.onyiah@gmail.com>
2010-03-24 11:25:41 +0000

    Not actually necessary here

--- lib/allele_image.rb
+++ lib/allele_image.rb
@@ -1,8 +1,6 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require "rubygems"
-
 # This entire module could do with a better name
 module AlleleImage
   VERSION = '0.0.2'
