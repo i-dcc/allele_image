b5f5465368188c9796f5ba43300b114022b0209d
Nelo Onyiah <io1@sanger.ac.uk>
2010-08-24 15:01:25 +0100

    Fix the tests to track the new directory layout [refs #2263]

--- test/test_coverage_failures.rb
+++ test/test_coverage_failures.rb
@@ -2,28 +2,14 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestCoverageFailures < Test::Unit::TestCase
   context 'An updated GenBank file' do
-    context 'for an allele' do
+    context 'with a deleted exon overlapping the rcmb primers' do
       setup do
-        @data_dir = File.dirname( __FILE__ ) + '/../misc/coverage-failures/allele/'
-        @alleles  = Dir.entries( @data_dir ).select { |entry| not [".", ".."].include?( entry ) }
+        @data_dir = File.dirname( __FILE__ ) + '/../misc/coverage-failures/fixnum-coercions'
       end
       
       should 'not have Fixnum coercion problems' do
-        @alleles.each do |file|
-          assert_not_nil AlleleImage::Image.new( @data_dir + file ), "#{ @data_dir + file } instantiates"
-        end
-      end
-    end
-
-    context 'for a vector' do
-      setup do
-        @data_dir = File.dirname( __FILE__ ) + '/../misc/coverage-failures/vector/'
-        @vectors  = Dir.entries( @data_dir ).select { |entry| not [".", ".."].include?( entry ) }
-      end
-      
-      should 'not have Fixnum coercion problems' do
-        @vectors.each do |file|
-          assert_not_nil AlleleImage::Image.new( @data_dir + file ), "#{ @data_dir + file } instantiates"
+        Dir["#{@data_dir}/*.gbk"].each do |file|
+          assert_not_nil AlleleImage::Image.new( file ), "#{ file } instantiates"
         end
       end
     end
