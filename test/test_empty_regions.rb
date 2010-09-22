be05d5d641ff910da48f63ba4fe743b6a8aa6dc6
Nelo Onyiah <io1@sanger.ac.uk>
2010-09-22 17:40:39 +0100

    Added some test stubs and updated the test data [refs #3898 #3899]

---
+++ test/test_empty_regions.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestEmptyRegions < Test::Unit::TestCase
+  context "An updated GenBank file" do
+    setup do
+      @base_dir = File.dirname( __FILE__ ) + '/../misc/known-issues/empty-regions'
+    end
+
+    [ 'allele', 'vector' ].each do |construct|
+      context "for an #{ construct } with empty spaces" do
+        setup do
+          @data_dir = @base_dir + "/#{ construct }"
+        end
+
+        should "" do
+          Dir["#{@data_dir}/*.gbk"].each do |file|
+            assert_not_nil AlleleImage::Image.new( file ), "#{ file } instantiates"
+          end
+        end
+      end
+    end
+  end
+end
