e2c2899ec6337da0993d38040f1103c316c08dcd
Nelo Onyiah <io1@sanger.ac.uk>
2010-09-23 16:39:39 +0100

    Add some tests for the height of the image we expect to generate [refs #1321]

--- test/test_missing_exons.rb
+++ test/test_missing_exons.rb
@@ -4,10 +4,10 @@ require 'pp'
 class TestMissingExons < Test::Unit::TestCase
   context 'An updated GenBank file' do
     setup do
-      @data_dir      = File.dirname( __FILE__ ) + '/../misc/known-issues/missing-exons'
-      @feature_count = {
-        "test/../misc/known-issues/missing-exons/12299.gbk" => 5,
-        "test/../misc/known-issues/missing-exons/10406.gbk" => 4
+      @data_dir = File.dirname( __FILE__ ) + '/../misc/known-issues/missing-exons'
+      @allele   = {
+        "test/../misc/known-issues/missing-exons/12299.gbk" => { :feature_count => 5, :height => 260 },
+        "test/../misc/known-issues/missing-exons/10406.gbk" => { :feature_count => 4, :height => 240 }
       }
     end
 
@@ -19,10 +19,17 @@ class TestMissingExons < Test::Unit::TestCase
           assert_not_nil allele_image, "#{ file } has an allele_image"
 
           three_flank_features = allele_image.construct.three_flank_features
-          assert_equal( @feature_count[file],
+          assert_equal( @allele[file][:feature_count],
             three_flank_features.size, "#{file} has the correct number of features" )
+          assert_equal( @allele[file][:height],
+            allele_image.render.rows, "#{file} has the correct height" )
           # allele_image.render.write( file.gsub( /\.\w+$/, '.png' ) )
-          # pp [ file => three_flank_features, :feature_count => @feature_count[file] ]
+          # pp [
+          #     file => [
+          #              :count => @allele[file][:feature_count],
+          #              :height => allele_image.render.rows
+          #             ]
+          #    ]
         end
       end
     end
