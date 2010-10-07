fb614b42d60c0d506caace89c1d1184d1674ba11
Nelo Onyiah <io1@sanger.ac.uk>
2010-10-07 16:27:45 +0100

    Update the tests to check the content of the cassette features [refs #3971]

--- test/test_functional_units.rb
+++ test/test_functional_units.rb
@@ -5,7 +5,22 @@ class TestFunctionalUnits < Test::Unit::TestCase
     setup do
       @data_dir  = "#{ File.dirname( __FILE__ ) }/../misc/known-issues/functional-units"
       @cassettes = {
-        "#{ @data_dir }/157.gbk" => { :label => "L1L2_gt0", :feature_count => 9 }
+        # list should include an example from every cassette
+        "#{ @data_dir }/157.gbk"   => {
+          :label => "L1L2_gt0",
+          :expected_features =>
+            ["FRT", "En2 SA", "T2A", "lacZ", "T2A", "neo", "pA", "FRT", "loxP"]
+        },
+        "#{ @data_dir }/6133.gbk"  => {
+          :label => "L1L2_Bact_EM7",
+          :expected_features =>
+            ["FRT", "En2 SA", "IRES", "lacZ", "pA", "loxP", "hBactP", "neo", "pA", "FRT", "loxP"]
+        },
+        "#{ @data_dir }/25075.gbk" => {
+          :label => "L1L2_hubi_P",
+          :expected_features =>
+            ["FRT", "En2 SA", "IRES", "lacZ", "pA", "loxP", "hubiP", "neo", "pA", "FRT", "loxP"]
+        }
       }
     end
 
@@ -15,9 +30,9 @@ class TestFunctionalUnits < Test::Unit::TestCase
 
         assert_not_nil allele_image, "#{ file } has an allele_image"
         assert_equal(
-          @cassettes[file][:feature_count],
-          allele_image.construct.cassette_features.size,
-          "#{ @cassettes[file][:label] } has the correct number of features"
+          @cassettes[file][:expected_features],
+          allele_image.construct.cassette_features.map { |feature| feature.feature_name },
+          "#{ @cassettes[file][:label] } has the correct features"
         )
       end
     end
