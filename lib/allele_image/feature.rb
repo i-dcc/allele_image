5f994049080f0ecfbd44ceba86a7bc060ff44b65
j1n3l0 <nelo.onyiah@gmail.com>
2010-10-28 11:44:04 +0100

    Updated AlleleImage::Feature to instantiate from a Bio::Feature [refs #112]

--- lib/allele_image/feature.rb
+++ lib/allele_image/feature.rb
@@ -1,15 +1,4 @@
 module AlleleImage
-  # == SYNOPSIS
-  #   feature = AlleleImage::Feature.new( feature_type, feature_name, start, stop )
-  #   feature.render( FORMAT )
-  #
-  # == DESCRIPTION
-  # A class to represent a feature of interest to us. The feature will
-  # only be instantiated if it is one of our RENDERABLE_FEATURES.
-  #
-  # My thoughts on the AlleleImage::Feature#render( FORMAT ) are not
-  # conclusive yet but I think I'm on the right track.
-  #
   class Feature
     @@text_width = 12
 
@@ -20,38 +9,50 @@ module AlleleImage
     attr_reader   :feature_type, :feature_name, :start, :stop, :render_options
     attr_accessor :orientation
 
-    def initialize( feature_type, feature_name, start, stop, orientation = "forward" )
-      unless renderable_feature?( feature_type, feature_name )
+    def initialize( bio_feature )
+      @feature_type = bio_feature.feature
+      @feature_name = bio_feature.qualifiers.first.value
+      @position     = bio_feature.position
+
+      init_orientation
+      init_start_and_stop
+
+      unless renderable_feature?
         raise "NotRenderable"
       end
 
-      @feature_type = feature_type
-      @start, @stop = start, stop
-      @orientation  = orientation
-
-      if feature_type == "exon" and not feature_name.match(/En2/)
-        @feature_name = feature_name
-      else
-        @render_options = AlleleImage::RENDERABLE_FEATURES[ feature_type ][ feature_name ]
-        @feature_name   = @render_options[ "label" ] || feature_name
-      end
+      # if @feature_type == "exon" and @feature_name.match(/En2/)
+      #   @render_options = AlleleImage::RENDERABLE_FEATURES[ @feature_type ][ @feature_name ]
+      #   @feature_name   = @render_options[ "label" ] || @feature_name
+      # end
     end
 
     def width
       @render_options and @render_options.has_key?("width") ? @render_options[ "width" ] :  @feature_name.length * @@text_width
     end
 
-    # def render( format )
-    #   # raise FeatureNotRenderable if RENDERABLE_FEATURES[ self.feature_type ][ self.feature_name ]
-    #   @renderer = eval( "RenderAs::#{ format }.new" )
-    #   @renderer.render_feature( self )
-    # end
-
     private
-      def renderable_feature?( feature_type, feature_name )
-        feature_type == "exon" or \
-        ( AlleleImage::RENDERABLE_FEATURES[ feature_type ] and \
-          AlleleImage::RENDERABLE_FEATURES[ feature_type ][ feature_name ] )
+      def renderable_feature?
+        @feature_type == "exon" or \
+        ( AlleleImage::RENDERABLE_FEATURES[ @feature_type ] and \
+          AlleleImage::RENDERABLE_FEATURES[ @feature_type ][ @feature_name ] )
+      end
+
+      def init_orientation
+        @orientation = @position.match(/^complement/) ? "reverse" : "forward"
+      end
+
+      def init_start_and_stop
+        regex     = Regexp.new(/-?\d+/)
+        matchdata = regex.match(@position)
+        locus     = []
+        while matchdata != nil
+          locus.push( matchdata[0].to_i )
+          @position = matchdata.post_match
+          matchdata = regex.match(@position)
+        end
+        @start = locus.first
+        @stop  = locus.last
       end
   end
 end
--- test/test_feature.rb
+++ test/test_feature.rb
@@ -1,6 +1,60 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestAlleleImageFeature < Test::Unit::TestCase
+  context "an AlleleImage::Feature" do
+    context "that is a primer" do
+      setup do
+        @features = [
+         { :type => "primer_bind", :pos => "72048..72067",           :name => "G3", :start => 72048,  :stop => 72067,  :ori => "forward" },
+         { :type => "primer_bind", :pos => "1925..1999",             :name => "HU", :start => 1925,   :stop => 1999,   :ori => "forward" },
+         { :type => "primer_bind", :pos => "complement(8085..8159)", :name => "HU", :start => 8085,   :stop => 8159,   :ori => "reverse" },
+         { :type => "primer_bind", :pos => "-36381..-36362",         :name => "G5", :start => -36381, :stop => -36362, :ori => "forward" },
+        ]
+      end
+
+      should "instantiate correctly from a Bio::Feature" do
+        @features.each do |feature|
+          bio_feature = Bio::Feature.new( feature[:type], feature[:pos] )
+          bio_feature.append( Bio::Feature::Qualifier.new( "note", feature[:name] ) )
+
+          # check a few basics truths ...
+          assert_not_nil bio_feature, "we have a bio feature"
+          assert_equal feature[:pos],  bio_feature.position, "get the position"
+          assert_equal feature[:name], bio_feature.qualifiers.first.value, "get the note"
+
+          ai_feature = AlleleImage::Feature.new( bio_feature )
+
+          # and now some basics *we hope are true* ...
+          assert_not_nil ai_feature, "we can instantiate an AlleleImage::Feature"
+          assert_equal feature[:type],  ai_feature.feature_type, "correct type"
+          assert_equal feature[:name],  ai_feature.feature_name, "correct name"
+          assert_equal feature[:start], ai_feature.start,        "correct start"
+          assert_equal feature[:stop],  ai_feature.stop,         "correct stop"
+          assert_equal feature[:ori],   ai_feature.orientation,  "correct orientation"
+        end
+      end
+    end
+
+    context "that is an exon" do
+      setup do
+        @features = []
+      end
+    end
+
+    context "that is not renderable" do
+      setup do
+        @features = []
+      end
+    end
+
+    context "that has render options" do
+      setup do
+        @features = []
+      end
+    end
+  end
+
+=begin
   context "a new AlleleImage::Feature" do
     setup do
       @feature = AlleleImage::Feature.new( "misc_feature", "loxP", 1000, 2000 )
@@ -62,4 +116,5 @@ class TestAlleleImageFeature < Test::Unit::TestCase
       end
     end
   end
+=end
 end
