75d9ab9f7c7df978c08fdf22068b57fcac478b02
j1n3l0 <nelo.onyiah@gmail.com>
2010-10-29 11:23:27 +0100

    Updated the tests [refs #112]

--- test/test_regeneron_products.rb
+++ test/test_regeneron_products.rb
@@ -1,24 +1,27 @@
 require File.dirname( __FILE__ ) + "/test_helper.rb"
 
 class TestRegeneronProducts < Test::Unit::TestCase
-  context "" do
+  context "a KOMP-Regeneron product" do
     setup do
       @data_dir  = "#{ File.dirname( __FILE__ ) }/../misc/regeneron"
-      @cassettes = [
-        { :label => "TM-ZEN-UB1", :file => "#{ @data_dir }/11723.gbk",
-          :features => [] },
-        { :label => "ZEN-Ub1",    :file => "#{ @data_dir }/12890.gbk",
-          :features => ["lacZ", "pA", "loxP", "hubiP", "neo", "pA", "pA", "loxP"] },
+      @cassettes = {
+        "ZEN-Ub1"    => [   "lacZ", "pA", "loxP", "hubiP", "neo", "pA", "loxP"],
+        "TM-ZEN-UB1" => ["TM-lacZ", "pA", "loxP", "hubiP", "neo", "pA", "loxP"],
+      }
+      @files = [
+        { :name => "#{ @data_dir }/10381.gbk", :cassette => "ZEN-Ub1"    },
+        { :name => "#{ @data_dir }/11723.gbk", :cassette => "TM-ZEN-UB1" },
+        { :name => "#{ @data_dir }/12591.gbk", :cassette => "TM-ZEN-UB1" },
+        { :name => "#{ @data_dir }/12890.gbk", :cassette => "ZEN-Ub1"    },
       ]
     end
 
-    should "" do
-      @cassettes.each do |product|
-        allele_image = AlleleImage::Image.new( product[:file] )
-        features = allele_image.construct.cassette_features.map { |f| f.feature_name }
-        allele_image.render.write( product[:file].gsub( /\.gbk$/, ".png" ) )
+    should "have the correct cassette features" do
+      @files.each do |file|
+        allele_image = AlleleImage::Image.new( file[:name] )
+        features     = allele_image.construct.cassette_features.map { |f| f.feature_name }
         assert_not_nil allele_image, "instantiate"
-        assert_equal product[:features], features, "have the expected features"
+        assert_equal @cassettes[ file[:cassette] ], features, "have the expected features"
       end
     end
   end
