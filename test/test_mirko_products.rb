175e10e2ac68697fc178e9349119de5c862e778f
j1n3l0 <nelo.onyiah@gmail.com>
2011-03-18 17:37:57 +0000

    Test that we can create, render and write an allele image to file [refs #1174]

--- test/test_mirko_products.rb
+++ test/test_mirko_products.rb
@@ -7,10 +7,14 @@ class TestMirKOProducts < Test::Unit::TestCase
       catch :done_testing do
         Dir["#{File.dirname(__FILE__)}/../misc/mirko/*.gbk"].each do |file|
           if File.size?(file)
-            assert_nothing_raised do
-              @allele_image = AlleleImage::Image.new(file)
+            ap "testing #{file}"
+            assert_nothing_raised { @allele_image = AlleleImage::Image.new(file) }
+            assert_nothing_raised { @magick_image = @allele_image.render }
+            assert_nothing_raised { @magick_image.write(file.gsub(/gbk$/, 'gif')) }
+            if @allele_image.construct.cassette_features.size > 0
+              ap "#{file} has #{@allele_image.construct.cassette_features.size} cassette features"
             end
-            throw :done_testing
+            # throw :done_testing
           end
         end
       end
