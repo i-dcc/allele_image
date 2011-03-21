6cf832f2126fa068640a1be81e82db5599571d58
j1n3l0 <nelo.onyiah@gmail.com>
2011-03-21 16:40:45 +0000

    Test the expected cassette names from a mirKO cassette [refs #1174].

--- test/test_mirko_products.rb
+++ test/test_mirko_products.rb
@@ -2,18 +2,19 @@ require 'test_helper'
 
 class TestMirKOProducts < Test::Unit::TestCase
   context 'A mirKO product' do
+    setup do
+      @cassette_features = ['LoxP', 'F3', 'PGK', 'pu-Delta-tk', 'pA', 'loxP', 'FRT']
+    end
+
     should 'render a valid image' do
       catch :done_testing do
         Dir["#{File.dirname(__FILE__)}/../misc/mirko/*.gbk"].each do |file|
           if File.size?(file)
-            ap "testing #{file}"
             assert_nothing_raised { @allele_image = AlleleImage::Image.new(file) }
             assert_nothing_raised { @magick_image = @allele_image.render }
             assert_nothing_raised { @magick_image.write(file.gsub(/gbk$/, 'gif')) }
-            if @allele_image.construct.cassette_features.size > 0
-              ap "#{file} has #{@allele_image.construct.cassette_features.size} cassette features"
-            end
-            # throw :done_testing
+            assert_equal @cassette_features, @allele_image.construct.cassette_features.map(&:feature_name)
+            throw :done_testing
           end
         end
       end
